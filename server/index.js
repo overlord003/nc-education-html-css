ca6dec64e3ca64c302a3b0cf9928fe77092057fa
overlord003 <lovtsova.christina@yandex.ru>
2021-06-11 16:01:53 +0300

    server append

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const app = express();
+app.get("*", express.static("./src"));
+
+const port = process.env.PORT || 8001;
+
+app.listen(port, () => {
+	console.log(`App start on port ${port}`);
+});
