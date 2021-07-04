00b31e58821e0afd53a07e7a7a1a6de683953032
overlord003 <lovtsova.christina@yandex.ru>
2021-07-05 01:27:05 +0300

    mail fixed (mb)

--- gulpfile.js
+++ gulpfile.js
@@ -72,7 +72,6 @@ gulp.task("serve", function () {
 
 	gulp.watch("./src/assets/styles/**/*.less").on("change", series("less"));
 	gulp.watch("./src/index.html").on("change", series("html"));
-
 	gulp.watch("./dist/style.css").on("change", browserSync.reload);
 	gulp.watch("./dist/index.html").on("change", browserSync.reload);
 });
